fbc9e964db11cb2f705b2d29d7ce97b02661fb88
Sidhanth <sidhanth.surana12@gmail.com>
2015-07-30 18:46:14 +0530

    commented

--- niki.rb
+++ niki.rb
@@ -4,6 +4,9 @@ require 'nickel'
 
 
 
+# there are times when people use past as last. The libraries dont understand last and so this make
+#the last to past
+
 
 def cut_short(text_to_process)
  inp=text_to_process.index 'for'
@@ -23,16 +26,24 @@ end
    return fs
 end
 
+
 puts 'Enter the text you want to process'
 
 text_to_process = gets
-s = 'Mon-Fri, Saturday, Sunday'
+
+s = 'Mon-Fri, Saturday, Sunday'#range of timings . can me made like Mon-Fri 19:00,sat 10:00
+
 c = ChronicBetween.new(s)
-#text_to_poces="before 5pm is good"
+
  text_to_process.downcase!
+
+
+
 if Chronic.parse(text_to_process) == nil
 if text_to_process.include?'for'
   puts 'its a period'
+
+
   inp=text_to_process.index 'for'
   s3= cut_short(text_to_process)
   s4="before"+s3
@@ -40,8 +51,14 @@ if text_to_process.include?'for'
   puts Chronic.parse(s4)
   puts 'to'
   puts Time.now
+  File.open('output.txt', 'a') { |file| file.write("\ntPeriod\t\t") }
+  File.open('output.txt', 'a') { |file| file.write(Chronic.parse(s4)) }
+    File.open('output.txt', 'a') { |file| file.write(" ") }
+  File.open('output.txt', 'a') { |file| file.write(Time.now) }
+
+
 end
-elsif Chronic.parse(text_to_process) == nil
+elsif Chronic.parse(text_to_process) != nil && (/before \d+/.match(text_to_process) || /after \d+/.match(text_to_process))
 #if c.within?DateTime.parse(text_to_process)
  puts 'it is a tTrigger'
  text_to_process.downcase!
@@ -51,20 +68,36 @@ elsif Chronic.parse(text_to_process) == nil
       puts Time.now
       puts 'end:'
       s2="today before"+text_to_process
+      s5=text_to_process
       n = Nickel.parse s2
+      if Time.now > n.occurrences[0].start_time.to_time
+        File.open('output.txt', 'a') { |file| file.write("\ntTrigger\t") }
+        File.open('output.txt', 'a') { |file| file.write("invalid trigger") }
+        puts n.occurrences[0].start_time.to_time
+      else
+
       puts n.occurrences[0].start_time.to_time
+      File.open('output.txt', 'a') { |file| file.write("\ntTrigger\t") }
+      File.open('output.txt', 'a') { |file| file.write(Time.now) }
+        File.open('output.txt', 'a') { |file| file.write(" ") }
+      File.open('output.txt', 'a') { |file| file.write(n.occurrences[0].start_time.to_time) }
+end
 else
   puts 'start:'
   s2="today after"+text_to_process
   n = Nickel.parse s2
   puts n.occurrences[0].start_time.to_time
+  File.open('output.txt', 'a') { |file| file.write("\ntTrigger\t") }
+  File.open('output.txt', 'a') { |file| file.write(n.occurrences[0].start_time.to_time) }
 
 end
 
 else
 
 #  puts 'the te is ' + text_to_process
-
+File.open('output.txt', 'a') { |file| file.write("\ntStamp\t\t") }
+puts 'its a timestamp'
   puts Chronic.parse(text_to_process)
+    File.open('output.txt', 'a') { |file| file.write(Chronic.parse(text_to_process)) }
 
 end
--- simple.rb
+++ simple.rb
@@ -4,7 +4,16 @@ require 'nickel'
 s1=" last two years"
 
 puts Chronic.parse(s1)
-
+text_to_process="before 5 pm"
 n = Nickel.parse "today before 5pm"
+puts 'listen '
+puts Chronic.parse("before 5pm the next day")
+
 puts n.occurrences[0].start_time
 puts n.occurrences[0].start_time.to_time
+if /before [0-9]/.match(text_to_process)
+puts 'Hola sid success'
+end
+if Chronic.parse(text_to_process) != nil && (/before [0-9]/.match(text_to_process) || /after [0-9]/.match(text_to_process))
+  puts 'Hola watha'
+end
